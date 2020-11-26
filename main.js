b289ac0429e3f72cafa16e62fdcb3c475cac19ce
Neliyenn <65779777+Neliyenn@users.noreply.github.com>
2020-11-26 17:35:09 +0100

    aj

---
+++ main.js
@@ -0,0 +1,30 @@
+const imgNames = [
+    'https://picsum.photos/id/1025/367/267',
+    'https://picsum.photos/id/1062/367/267',
+    'https://picsum.photos/id/237/367/267',
+    'https://picsum.photos/id/433/367/267',
+    'https://picsum.photos/id/659/367/267'
+];
+
+const images = [];
+var gallery = document.querySelector('.gallery');
+
+function startLoading() 
+{
+    imgNames.forEach(name => {
+        const img = new Image();
+
+        img.addEventListener('load', e => {
+            images.push(img);
+            gallery.appendChild(img);
+        });
+
+        img.src = name;
+        if(img.complete)
+        {
+            img.addEventListener(new Event('load'));
+        }
+    });
+}
+
+startLoading();
