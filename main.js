1a188fdd8b6dae67ed860c3f35cd7b0180f9e0b7
MatGalos <67073319+MatGalos@users.noreply.github.com>
2020-11-26 19:30:38 +0100

    Dynamic gallery ended

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const imgNamesAJ = [
 ];
 
 const imagesAJ = [];
-var gallery = document.querySelector('.gallery');
+const galleryAJ = document.getElementById('AJ');
 
 function startLoadingAJ() 
 {
@@ -16,7 +16,7 @@ function startLoadingAJ()
  
         imgAJ.addEventListener('load', e => {
             imagesAJ.push(imgAJ);
-            gallery.appendChild(imgAJ);
+            galleryAJ.appendChild(imgAJ);
         });
 
  
@@ -28,17 +28,44 @@ function startLoadingAJ()
     });
 }
 startLoadingAJ();
-var gallery = document.querySelector('.gallery');
 
 
+const djGal = document.getElementById('DJ');
+const djNames = [
+    'https://picsum.photos/id/237/400/300',
+    'https://picsum.photos/id/238/400/300',
+    'https://picsum.photos/id/239/400/300',
+    'https://picsum.photos/id/240/400/300',
+    'https://picsum.photos/id/241/400/300',
+];
+
+function startLoadingDJ() 
+{
+    djNames.forEach(djEl => {
+        const imgDJ = new Image();
+ 
+        imgDJ.addEventListener('load', () => {
+            djGal.appendChild(imgDJ);
+        });
+
+ 
+        imgDJ.src = djEl;
+        if(imgDJ.complete)
+        {
+            imgDJ.addEventListener(new Event('load'));
+        }
+    });
+}
+startLoadingDJ();
+
+
+const galleryPB = document.getElementById('PB');
 const imagesAr = [
     'http://lorempixel.com/400/200/sports/1/',
     'http://lorempixel.com/400/200/sports/2/',
     'http://lorempixel.com/400/200/sports/5/',
     'http://lorempixel.com/400/200/sports/6/',
-    'http://lorempixel.com/400/200/sports/9/',
-    'http://lorempixel.com/400/200/sports/7/',
-    'http://lorempixel.com/400/200/sports/3/'
+    'http://lorempixel.com/400/200/sports/9/'
 ];
 
 const imagesPB = [];
@@ -49,7 +76,7 @@ function startLoadingPB() {
 
         imgPB.addEventListener('load', e => {
             imagesPB.push(imgPB);
-            gallery.appendChild(imgPB);   });
+            galleryPB.appendChild(imgPB);   });
         imgPB.src = imagePB;
 
         if (imgPB.complete) {
@@ -59,3 +86,29 @@ function startLoadingPB() {
 }
 
 startLoadingPB();
+
+const galleryMG=document.getElementById('MG');
+const imagesMG=[
+            'https://picsum.photos/id/1/500/800',
+            'https://picsum.photos/id/2/500/800',
+            'https://picsum.photos/id/3/500/800',
+            'https://picsum.photos/id/4/500/800',
+            'https://picsum.photos/id/5/500/800'
+    ]
+
+    function startLoadingMG() {
+        imagesMG.forEach(imageMG => {
+            const imgMG = new Image();
+    
+            imgMG.addEventListener('load', e => {
+                imagesMG.push(imgMG);
+                galleryMG.appendChild(imgMG);   });
+            imgMG.src = imageMG;
+    
+            if (imgMG.complete) {
+                imgMG.addEventListener(new Event('load'));
+            }
+        });
+    }
+    
+    startLoadingMG();
